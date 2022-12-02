fab8d3bc8ac1c856820b9d3ae0f1cd8950482dcd
Joshua Harris <jharrisjoshua@gmail.com>
2022-12-02 08:29:55 -0500

    Day 1

---
+++ 1-5/AOC22_D1_A.py
@@ -0,0 +1,12 @@
+def max_food():
+    with open("AOC_D1_A_input.txt", 'r') as infile:
+        max_sum, run_sum = 0, 0
+        for line in infile:
+            if len((nice_line := line.strip())) == 0:
+                max_sum, run_sum = max(max_sum, run_sum), 0
+            else:
+                run_sum += int(nice_line)
+    return max_sum
+
+
+print(max_food())
---
+++ 1-5/AOC22_D1_B.py
@@ -0,0 +1,24 @@
+def max_food():
+    with open("AOC_D1_A_input.txt", 'r') as infile:
+        run_sum, hangry_elves = 0, []
+        for line in infile:
+            if len((clean_line := line.strip())) == 0:
+                top_three(hangry_elves, run_sum)
+                run_sum = 0
+            else:
+                run_sum += int(clean_line)
+    return sum(hangry_elves)
+
+
+def top_three(big_boys, contender):
+    if len(big_boys) < 3:
+        big_boys.append(contender)
+    else:
+        if contender > big_boys[-1]:
+            big_boys[-1] = contender
+        else:
+            return  # from whence you came, little elf
+    big_boys.sort(reverse=True)
+
+
+print(max_food())

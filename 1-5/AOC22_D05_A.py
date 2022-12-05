95b0b0e35604e7071642533de4f6b35550e11a26
Josh Harris <jharrisjoshua@gmail.com>
2022-12-05 12:30:45 -0500

    Day 5 - refactor

--- 1-5/AOC22_D05_A.py
+++ 1-5/AOC22_D05_A.py
@@ -1,29 +1,33 @@
 from collections import deque
 
 
-def main(file_name):
-    with open(file_name, 'r') as infile:
-        stacks = deque([])
-        for line in infile:
-            if '[' in line:
-                line = line.replace('    ', '0')
-                line = [char for char in line if char not in [' ', '[', ']', '\n']]
-                if not stacks:
-                    stacks = [deque([]) for _ in range(len(line))]
-                for i, val in enumerate(line):
-                    if len(stacks) == i:
-                        stacks.append(deque([]))
-                    if val != '0':
-                        stacks[i].appendleft(val)
-            if 'move' in line:
-                move = [int(num) for num in line.strip().split(' ') if num.isdigit()]
-                for i in range(move[0]):
-                    if stacks[move[1] - 1]:
-                        stacks[move[2] - 1].append(stacks[move[1] - 1].pop())
-
-        result = [stack[-1] for stack in stacks if stack]
-
-        return ''.join(result)
+def stackin_toys(on_santas_list):
+    with open(on_santas_list, 'r') as santas_shop:
+        in_stacks = []
+        for toys in santas_shop:
+            if '[' in toys:
+                elves_stack(toys, in_stacks)
+            if 'move' in toys:
+                elves_shuffle(toys, in_stacks)
+        top_shelf_toys = [toy_stack[-1] for toy_stack in in_stacks if toy_stack]
+        return ''.join(top_shelf_toys)
 
 
-print(main("AOC22_D05_inp.txt"))
+def elves_stack(toys, in_toy_stacks):
+    santa_r_us_aisle_num = 0
+    for i, toy in enumerate(toys):
+        if i % 4 == 1:
+            santa_r_us_aisle_num += 1
+            if len(in_toy_stacks) < santa_r_us_aisle_num:
+                in_toy_stacks.append(deque([]))
+            if toy.isalpha():
+                in_toy_stacks[santa_r_us_aisle_num - 1].appendleft(toy)
+
+
+def elves_shuffle(toys, in_stacks):
+    move = [int(num) for num in toys.strip().split(' ') if num.isdigit()]
+    for _ in range(move[0]):
+        in_stacks[move[2] - 1].append(in_stacks[move[1] - 1].pop())
+
+
+print(stackin_toys("AOC22_D05_inp.txt"))
